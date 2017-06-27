a274aefcf4452812c33a1661a79d8f89a8072f13
israinbow <18473481681@163.com>
2017-06-27 15:39:46 +0800

    修改了首页和部分数据库

--- routers/main.js
+++ routers/main.js
@@ -8,7 +8,7 @@ var mysql=require("mysql");
 var pool=mysql.createPool({
     host:"127.0.0.1",
     port:3306,
-    database:"myblog",
+    database:"blog",
     user:"root",
     password:"aaaa"
 });
--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(session({
 var pool=mysql.createPool({
     host:"127.0.0.1",
     port:3306,
-    database:"myblog",
+    database:"blog",
     user:"root",
     password:"aaaa"
 });
